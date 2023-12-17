c239ab2f89ac4d924360f8facbba2e9424e65cbf
Chris Bennett <110393183+oev-chrisbennett@users.noreply.github.com>
2023-12-17 23:41:01 +0000

    stage 2

--- app/main.py
+++ app/main.py
@@ -7,11 +7,13 @@ def main() -> None:
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, client_address = server_socket.accept()
 
-    client_socket.recv(1024) # Buffer size of 1024 bytes
+    request = client_socket.recv(1024).decode()
 
-    response = "HTTP/1.1 200 OK\r\n\r\n"
-    client_socket.sendall(response.encode())
-
+    first_line = request.split("\n")[0]
+    method, path, protocol = first_line.split(" ")
+    response = b"HTTP/1.1 200 OK\r\n\r\n" if path == "/" else b"HTTP/1.1 404 Not Found\r\n\r\n"
+    print(f"Response: {response}")
+    client_socket.sendall(response)
     client_socket.close()
 
 
