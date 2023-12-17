fb6e56595282ecd1cd674899c23f267324165a62
Chris Bennett <110393183+oev-chrisbennett@users.noreply.github.com>
2023-12-17 23:31:06 +0000

    stage 2

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 import socket
 
 
-def main():
-
-    print("Logs from your program will appear here!")
-
-
+def main() -> None:
+    print("Server is running on port 4221")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
+    client_socket, client_address = server_socket.accept()
+
+    client_socket.recv(1024) # Buffer size of 1024 bytes
+
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.sendall(response.encode())
+
+    client_socket.close()
 
 
 if __name__ == "__main__":
